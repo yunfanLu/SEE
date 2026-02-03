4097dab4c5d5df9b3a60a58c7f754299ad8eb688
yunfanlLu <yunfanlu913@outlook.com>
2026-02-03 16:33:47 +0800

    [ADD] Add configs

---
+++ options/SDE/SEENet_SDE.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#SBATCH -N 1 # 指定node的数量
+#SBATCH -p i64m1tga800u #  i64m1tga40u, i64m1tga800u
+#SBATCH --gres=gpu:1 # 需要使用多少GPU，n是需要的数量
+#SBATCH --time=7-00:00:00 # 指定任务运⾏的上限时间（我们的HPC为7天
+#SBATCH --job-name=SEENet_SDE-ae2-96-4-8-4-loss-2-epoch-20
+#SBATCH --output=./logs/SeeDynamicEventDataset/SEENet/Ablation-Cascade/SEENet_SDE-ae3-96-4-8-4-loss-2-epoch-20-using-cascade/%j-job.out # slurm的输出文件，%j是jobid
+#SBATCH --error=./logs/SeeDynamicEventDataset/SEENet/Ablation-Cascade/SEENet_SDE-ae3-96-4-8-4-loss-2-epoch-20-using-cascade/%j-job.err # 指定错误输出的格式
+#SBATCH --cpus-per-task=1
+
+export CUDA_VISIBLE_DEVICES="0"
+export PATH="/hpc2hdd/home/ylu066/miniconda3/bin":$PATH
+export PYTHONPATH="./3rdparty/":$PYTHONPATH
+export PYTHONPATH="./":$PYTHONPATH
+
+echo "Job start at $(date "+%Y-%m-%d %H:%M:%S")"
+echo "Job run at:"
+echo "$(hostnamectl)"
+echo "Python version: $(python --version)"
+echo "              : $(which python)"
+echo "CUDA version: $(nvcc --version)"
+nvidia-smi
+
+
+python see/main.py \
+  --yaml_file="options/SDE/SEENet_SDE.yaml" \
+  --log_dir="./logs/SDE/SEENet_SDE/" \
+  --alsologtostderr=True
+
+
+
